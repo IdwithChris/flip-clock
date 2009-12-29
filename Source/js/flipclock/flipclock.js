607b83c0a4669631c618211ff34fa332db9a7830
sanchosrancho <sanchosrancho@2f912842-f45a-11de-9a6a-9d1f184cc70b>
2009-12-29 09:47:10 +0000

    Temporary sources

---
+++ Source/js/flipclock/flipclock.js
@@ -0,0 +1,15 @@
+(function($) {	
+	$.FlipClock = {
+		version: '0.0.1',
+
+		defaultConf: {}
+	};
+	
+	$.fn.FlipClock = function(conf) {
+		var conf = $.extend($.FlipClock.defaultConf, conf);
+		var self = this, $this = $(this);
+		
+		
+	};		
+		
+}) (jQuery); 
